5ce693455f0747b21cf33b7a7a903d23cf12ab8d
arben-djokovic <benidjokovic12@gmail.com>
2024-05-08 17:50:06 +0200

    Deleted unnessecery things

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,29 +1,29 @@
-import React from 'react'
-import { PrivateRoute } from '../../services/PrivateRoute'
-import { auth } from '../../services/AuthService';
-import apiCalls from "../../api/apiCalls"; 
-import { ToastContainer, toast } from 'react-toastify';
-import { useHistory } from 'react-router-dom';
+import React from "react";
+import { PrivateRoute } from "../../services/PrivateRoute";
+import { auth } from "../../services/AuthService";
+import apiCalls from "../../api/apiCalls";
+import { useHistory } from "react-router-dom";
 
 export default function Home() {
-    const history = useHistory();
-    
-    const logOut = async() => {
-        try{
-          const response = await apiCalls.post("/user/logout");
-          toast.success("Logged out");
-          auth.logout()
-          history.push("/log-in");
-        }catch(err){
-            toast.error(err.response.data.responseCodes[0].responseKey)
-        }
-      }
-  return (<PrivateRoute>
-        <div>
-            <h1>You are logged!!</h1>
-            <p>Username: {localStorage.getItem("username")}</p>
-            <button onClick={logOut}>LOGOUT</button>
-        </div>
+  const history = useHistory();
+
+  const logOut = async () => {
+    try {
+      const response = await apiCalls.post("/user/logout");
+      toast.success("Logged out");
+      auth.logout();
+      history.push("/log-in");
+    } catch (err) {
+      toast.error(err.response.data.responseCodes[0].responseKey);
+    }
+  };
+  return (
+    <PrivateRoute>
+      <div>
+        <h1>You are logged!!</h1>
+        <p>Username: {localStorage.getItem("username")}</p>
+        <button onClick={logOut}>LOGOUT</button>
+      </div>
     </PrivateRoute>
-  )
+  );
 }
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,8 +1,7 @@
 import React, { useState } from "react";
 import "./Login.scss";
-import apiCalls from "../../api/apiCalls"; 
+import apiCalls from "../../api/apiCalls";
 import { useHistory } from "react-router-dom";
-import { ToastContainer, toast } from "react-toastify";
 import { auth } from "../../services/AuthService";
 import { GuestRoute } from "../../services/GuestRoute";
 
@@ -19,48 +18,53 @@ export default function Login() {
       const userInfos = {
         requestBody: {
           username: usernameInput,
-          password: passwordInput
-        }
+          password: passwordInput,
+        },
       };
       try {
-        const response = await apiCalls.post("/user/login", userInfos); 
-        if(response.data.success){
-          const token = response.headers['24ca04apr02'];
-          auth.login(token, usernameInput)
-          toast.success("You are logged!")
+        const response = await apiCalls.post("/user/login", userInfos);
+        if (response.data && response.data.success) {
+          const token = response.headers["24ca04apr02"];
+          auth.login(token, usernameInput);
+          toast.success("You are logged!");
           history.push("/");
-        };
+        }
       } catch (error) {
-        toast.error(error.response.data.responseCodes[0].responseKey);
+        console.log(error);
+        if (error.response) {
+          toast.error(error.response.data.responseCodes[0].responseKey);
+        }
       }
     }
   };
 
-  return (<GuestRoute>
-    <div className="authPage">
-      <form className="authForm">
-        <div className="input">
-          <p>Username</p>
-          <input
-            value={usernameInput}
-            onChange={(e) => setUsernameInput(e.target.value)}
-            type="text"
-            name="username"
-          />
-        </div>
-        <div className="input">
-          <p>Password</p>
-          <input
-            value={passwordInput}
-            onChange={(e) => setPasswordInput(e.target.value)}
-            type="password"
-            name="password"
-          />
-        </div>
-        <button className="loginBtn" onClick={loginFun}>
-          Log in
-        </button>
-      </form>
-    </div></GuestRoute>
+  return (
+    <GuestRoute>
+      <div className="authPage">
+        <form className="authForm">
+          <div className="input">
+            <p>Username</p>
+            <input
+              value={usernameInput}
+              onChange={(e) => setUsernameInput(e.target.value)}
+              type="text"
+              name="username"
+            />
+          </div>
+          <div className="input">
+            <p>Password</p>
+            <input
+              value={passwordInput}
+              onChange={(e) => setPasswordInput(e.target.value)}
+              type="password"
+              name="password"
+            />
+          </div>
+          <button className="loginBtn" onClick={loginFun}>
+            Log in
+          </button>
+        </form>
+      </div>
+    </GuestRoute>
   );
 }
